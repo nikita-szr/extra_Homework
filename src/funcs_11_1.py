68c35f2916ac23d065682df31e32e3c894d794a8
Nikita Osipov <Nikittaosipov@yandex.ru>
2024-05-19 14:16:25 +0400

    fix by comment

--- src/funcs_11_1.py
+++ src/funcs_11_1.py
@@ -1,14 +1,12 @@
 def non_empty_truths(input_list):
-    return [[i for i in list if i and i != []]
-        for list in input_list
-        if list]
+    return [[i for i in list if i and i != []]for list in input_list if list]
 
 
-print(non_empty_truths([]))  # []
-print(non_empty_truths([[], []]))  # []
-print(non_empty_truths([[0]]))  # []
-print(non_empty_truths([[0, ""], [False, None]]))  # []
-print(non_empty_truths([[0, 1, 2], [], [], [False, True, 42]]))  # [[1, 2], [True, 42]]
+# print(non_empty_truths([]))  # []
+# print(non_empty_truths([[], []]))  # []
+# print(non_empty_truths([[0]]))  # []
+# print(non_empty_truths([[0, ""], [False, None]]))  # []
+# print(non_empty_truths([[0, 1, 2], [], [], [False, True, 42]]))  # [[1, 2], [True, 42]]
 
 
 def my_map(f, xs):
@@ -16,11 +14,13 @@ def my_map(f, xs):
     for x in xs:
         yield f(x)
 
+
 def my_filter(f, xs):
     """Упрощенная версия filter"""
     for x in xs:
         if f(x):
             yield x
+
 
 def replicate_each(n, xs):
     """Для каждого элемента итератора xs выдает на выход по n копий этого элемента"""
